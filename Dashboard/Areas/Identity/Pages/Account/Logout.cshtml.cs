5ef8bbca5e8e6dcae607325d8d0191e4acba7851
Sibusiso Shongwe <sibusisoshongwe1994@gmail.com>
2023-11-30 19:19:30 +0200

    Lot issue Temprary Fix

--- Dashboard/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Dashboard/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Dashboard.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,19 +15,19 @@ namespace Dashboard.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ILogger<AppUser> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<AppUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
-            _logger = logger;
+           // _logger = logger;
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
+            //_logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
--- Dashboard/Controllers/UsersController.cs
+++ Dashboard/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Dashboard.Controllers
 {
